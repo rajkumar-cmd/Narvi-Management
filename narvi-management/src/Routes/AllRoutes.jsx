7da6022feed1bcff836b6c5045d00025cd56e3c0
rajkumar-cmd <raymarhossor@gmail.com>
2022-12-15 22:09:58 +0530

    completed navbar and footer

--- narvi-management/src/Routes/AllRoutes.jsx
+++ narvi-management/src/Routes/AllRoutes.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import {Routes,Route} from "react-router-dom"
 import Home from "./Home"
-import ResourceHub from "./ResourceHub"
 import Pricing from "./Pricing"
 import Contact from "./ContactSales"
 import Login from "./Login"
@@ -11,7 +10,6 @@ const AllRoutes = () => {
   return (
     <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/resourcehub" element={<ResourceHub/>}/>
         <Route path="/pricing" element={<Pricing/>}/>
         <Route path="/contact" element={<Contact/>}/>
         <Route path="/login" element={<Login/>}/>
--- narvi-management/src/Routes/ResourceHub.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ResourceHub = () => {
-  return (
-    <div>ResourceHub</div>
-  )
-}
-
-export default ResourceHub
